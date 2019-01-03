ce7953d11843ff81473247f58b43690ea33ae9ee
Samuel Bassett <samabassett@gmail.com>
2019-01-03 12:23:29 +0100

    Updates

---
+++ precache-manifest.186e05cec26de00f8149341125daf477.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cdussuelwebsite2/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "dd78b6f3f0d5cd9b61ea",
+    "url": "/cdussuelwebsite2/static/js/runtime~main.dd78b6f3.js"
+  },
+  {
+    "revision": "02852194489e7e089d01",
+    "url": "/cdussuelwebsite2/static/js/main.02852194.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/cdussuelwebsite2/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "02852194489e7e089d01",
+    "url": "/cdussuelwebsite2/static/css/main.99ce7ad3.chunk.css"
+  },
+  {
+    "revision": "492ae51c3b5e54237275a0b4c9eaed95",
+    "url": "/cdussuelwebsite2/index.html"
+  }
+];
