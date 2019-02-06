a26c08e11222bdd5574ea4aab62818b40c273375
Samuel Bassett <samabassett@gmail.com>
2019-02-06 21:59:08 +0100

    Updates

---
+++ precache-manifest.ca12412698fad8c72f4866f09a7f5cb0.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "507d627babb6d8736184a14762cfcd23",
+    "url": "/cdussuelwebsite2/static/media/insta-icon.507d627b.png"
+  },
+  {
+    "revision": "95806881b89f1e10cb7e",
+    "url": "/cdussuelwebsite2/static/css/main.e304e6cb.chunk.css"
+  },
+  {
+    "revision": "ea10844cf16578aa08b5d71886247b36",
+    "url": "/cdussuelwebsite2/static/media/Lack-Regular.ea10844c.ttf"
+  },
+  {
+    "revision": "001374954bce056a9d5c",
+    "url": "/cdussuelwebsite2/static/js/1.00137495.chunk.js"
+  },
+  {
+    "revision": "dd78b6f3f0d5cd9b61ea",
+    "url": "/cdussuelwebsite2/static/js/runtime~main.dd78b6f3.js"
+  },
+  {
+    "revision": "617ce412e010b9d0691c679f25f7e0cd",
+    "url": "/cdussuelwebsite2/static/media/info.617ce412.svg"
+  },
+  {
+    "revision": "95806881b89f1e10cb7e",
+    "url": "/cdussuelwebsite2/static/js/main.95806881.chunk.js"
+  },
+  {
+    "revision": "21ba0040bae49f616016f93d817574c9",
+    "url": "/cdussuelwebsite2/static/media/Lack-Regular.21ba0040.woff"
+  },
+  {
+    "revision": "557f82420caf828ac6dd15573c79ee06",
+    "url": "/cdussuelwebsite2/static/media/Lack-Regular.557f8242.eot"
+  },
+  {
+    "revision": "2ba06ebe0f91838cd994b452264f3d54",
+    "url": "/cdussuelwebsite2/static/media/Lack-Regular.2ba06ebe.woff2"
+  },
+  {
+    "revision": "001374954bce056a9d5c",
+    "url": "/cdussuelwebsite2/static/css/1.79475640.chunk.css"
+  },
+  {
+    "revision": "8d82bdf4cee05ee0fe2dcd37faf470ab",
+    "url": "/cdussuelwebsite2/index.html"
+  }
+];
