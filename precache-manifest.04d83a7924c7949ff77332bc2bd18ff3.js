82579f5747086074ed319f993e8eb78e8d69a913
Samuel Bassett <samabassett@gmail.com>
2019-02-06 22:03:03 +0100

    Updates

---
+++ precache-manifest.04d83a7924c7949ff77332bc2bd18ff3.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "507d627babb6d8736184a14762cfcd23",
+    "url": "/static/media/insta-icon.507d627b.png"
+  },
+  {
+    "revision": "0d701898349576d18fa6",
+    "url": "/static/css/main.bb34f24f.chunk.css"
+  },
+  {
+    "revision": "ea10844cf16578aa08b5d71886247b36",
+    "url": "/static/media/Lack-Regular.ea10844c.ttf"
+  },
+  {
+    "revision": "001374954bce056a9d5c",
+    "url": "/static/js/1.00137495.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "617ce412e010b9d0691c679f25f7e0cd",
+    "url": "/static/media/info.617ce412.svg"
+  },
+  {
+    "revision": "0d701898349576d18fa6",
+    "url": "/static/js/main.0d701898.chunk.js"
+  },
+  {
+    "revision": "557f82420caf828ac6dd15573c79ee06",
+    "url": "/static/media/Lack-Regular.557f8242.eot"
+  },
+  {
+    "revision": "2ba06ebe0f91838cd994b452264f3d54",
+    "url": "/static/media/Lack-Regular.2ba06ebe.woff2"
+  },
+  {
+    "revision": "21ba0040bae49f616016f93d817574c9",
+    "url": "/static/media/Lack-Regular.21ba0040.woff"
+  },
+  {
+    "revision": "001374954bce056a9d5c",
+    "url": "/static/css/1.79475640.chunk.css"
+  },
+  {
+    "revision": "d0528a6753fc7b7ff5edeec60c4455b4",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/cdussuelwebsite2/precache-manifest.ca12412698fad8c72f4866f09a7f5cb0.js"
+  "/precache-manifest.04d83a7924c7949ff77332bc2bd18ff3.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/cdussuelwebsite2/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
